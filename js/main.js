03ad22afae3ffd960ab96956034e0b7bbcf047de
Gary JULIEN <garyjulien.aeqg@gmail.com>
2022-03-09 21:59:26 +0100

    - Handle responsive

--- js/main.js
+++ js/main.js
@@ -19,7 +19,9 @@ const soundBarsElt = logoElt.querySelector('.sound-bars');
 let yOffset = 0;
 let flowAnimation = 0;
 const originalSizesFrequenceBar = [
-    37.5, 56, 47.5, 37.5, 29, 65, 57, 53, 47, 34, 58, 74, 63, 61, 52, 48, 44, 39, 34, 64, 74, 53, 46.5, 43, 46.5, 43, 37, 17, 74, 26, 27.5, 43.5, 53.5, 59, 64, 66.5, 56.5, 52.5, 47.5, 42.5, 35, 59, 54, 44.5, 31.5, 28, 32.5, 47, 56, 37];
+    37.5, 56, 47.5, 37.5, 29, 65, 57, 53, 47, 34, 58, 74, 63, 61, 52, 48, 44, 39, 34, 64, 74, 53, 46.5, 43, 46.5, 43, 37,
+    17, 74, 26, 27.5, 43.5, 53.5, 59, 64, 66.5, 56.5, 52.5, 47.5, 42.5, 35, 59, 54, 44.5, 31.5, 28, 32.5, 47, 56, 37
+];
 let cloneSizesFrequenceBar = [...originalSizesFrequenceBar];
 
 /**
@@ -52,7 +54,7 @@ const animateLogo = () => {
         resetLogoVariables();
         createFrequencyElts();
     } else {
-        if (flowAnimation % 2 === 0) {
+        if (flowAnimation % 0.5 === 0) {
             if (yOffset < window.scrollY) {
                 const lastItem = cloneSizesFrequenceBar.pop();
                 cloneSizesFrequenceBar.unshift(lastItem);
@@ -63,30 +65,31 @@ const animateLogo = () => {
             createFrequencyElts();
         }
         yOffset = window.scrollY;
-        flowAnimation++;
+        flowAnimation += 0.5;
     }
 }
 
 /**
  * GSAP ANIMATIONS
  */
+const isMobile = window.innerWidth < 767;
 
 const tl = gsap.timeline({
     scrollTrigger: {
         trigger: ".page-logo",
-        start: "top top",
+        start: "-30% top",
         end: "+=100%",
         toggleAction: "play none reverse none",
-        scrub: 1,
-        markers: true
+        scrub: 1.5,
+        markers: true,
     }
 });
+
 tl.to(".page-logo", {
-    scale: 0.8,
+    scale: 0.5,
     margin: 0,
-    x: 0,
-    y: 0,
-    padding: 0
+    x: isMobile ? "30%" : "240%",
+    y: "-130%"
 });
 
 
